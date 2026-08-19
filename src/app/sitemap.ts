6bd54d0477d3ca86c19d706cc9d9d4ddbcf01385
Ezequiel Oliveira <ezequieloliveiraes@outlook.com>
2026-08-18 22:48:53 -0300

    feat(index) indexation

---
+++ src/app/sitemap.ts
@@ -0,0 +1,26 @@
+// app/sitemap.ts
+
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.altamedtecnologia.com.br',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.altamedtecnologia.com.br/quemsomos',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.altamedtecnologia.com.br/produtos',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+  ]
+}
